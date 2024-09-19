367123a1458a8853c359f08cd17454f158e626a8
LAlenaU <alenka19949@gmail.com>
2024-09-19 22:14:22 +0300

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,5 @@
+number=12
+hour_work=1.5
+name_course='Python'
+time_work=hour_work/number
+print('Курс:',name_course,', всего задач' , number,', затрачено часов', hour_work, ', среднее время выполнения', time_work, 'часа')
